654a436886838198a97c37add9329d30c1cd64d6
Denis <vin_ogr@list.ru>
2024-05-05 22:57:56 +0700

    Initial commit

---
+++ addwork.py
@@ -0,0 +1,11 @@
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+
+for (index) in data_structure:
+    print(type(index))
