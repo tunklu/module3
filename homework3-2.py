6a105ba25de28a4b20e847a6368f3959e9386186
Denis <vin_ogr@list.ru>
2024-05-12 22:59:24 +0700

    Распаковка параметров и параметры функции

--- homework3-2.py
+++ homework3-2.py
@@ -1,6 +1,13 @@
 def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
+
+values_list = [13, 'string', 15.5]
+values_dict = {'a': 17, 'b': 'текст', 'c': False}
+values_list_2 = [True, 234.34]
+
 print_params()
 print_params(b = 25)
-print_params(c = [1,2,3])
-values_list = [13,'string', 15.5]
+print_params(c = [1, 2, 3])
+print_params(* values_list)
+print_params(** values_dict)
+print_params(* values_list_2, 42)
