ca8ed344e4520c88098f994c4439f6170d9521f9
Denis <vin_ogr@list.ru>
2024-05-17 23:21:57 +0700

    addtask working

--- addwork.py
+++ addwork.py
@@ -1,3 +1,6 @@
+def calculate_structure_sum(*args):
+    return sum(calculate_structure_sum(*a) if hasattr(a, '__iter__') else (type(a) in (int, float)) (a) for a in args)
+
 data_structure = [
     [1, 2, 3],
     {'a': 4, 'b': 5},
@@ -6,6 +9,7 @@ data_structure = [
     ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
+result = calculate_structure_sum(data_structure)
+print(result)
 
-for (index) in data_structure:
-    print(type(index))
+
