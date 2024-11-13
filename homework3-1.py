0ad9ce14eec5ddfa68042477d5c0e698a093dc6a
Denis <vin_ogr@list.ru>
2024-11-13 10:17:03 +0700

    Пространство имён

--- homework3-1.py
+++ homework3-1.py
@@ -1,12 +1,22 @@
-def test():
-    a = 12
-    b = 'Monkeys'
-    print(a, b)
+from operator import length_hint
 
+calls = 0
 
-def test2(a, b, c):
-    print(a, b, c)
+def count_calls ():
+    global calls
+    calls+=1
 
+def string_info (string):
+    count_calls()
+    inforamtion = len(string), string.upper(), string.lower()
+    return inforamtion
 
-test()
-test2(11, 'друзей', 'Оушена')
+def is_contains (string, list_to_search):
+    count_calls()
+    return string.upper() in [s.upper() for s in list_to_search]
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBan
+print(is_contains('cycle', ['recycle', 'cyclic'])) # No matches
+print(calls)
