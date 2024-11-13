5d16bf963498bf0c86535f8c60ec64f70fd510a6
Denis <vin_ogr@list.ru>
2024-11-13 13:43:10 +0700

    Способы вызова функции

--- homework3-2.py
+++ homework3-2.py
@@ -1,13 +1,15 @@
-def print_params(a = 1, b = 'строка', c = True):
-    print(a, b, c)
+def send_email(message, recipient,sender = 'university.help@gmail.com'):
+    if ('@' not in recipient or '@' not in sender or not recipient.endswith((".com", ".ru", ".net"))
+            or not sender.endswith((".com", ".ru", ".net"))):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif recipient == sender:
+        print(f'Нельзя отправить письмо самому себе')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    elif sender != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
 
-values_list = [13, 'string', 15.5]
-values_dict = {'a': 17, 'b': 'текст', 'c': False}
-values_list_2 = [True, 234.34]
-
-print_params()
-print_params(b = 25)
-print_params(c = [1, 2, 3])
-print_params(* values_list)
-print_params(** values_dict)
-print_params(* values_list_2, 42)
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
