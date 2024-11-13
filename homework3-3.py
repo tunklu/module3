d36dd55eead11c0339bf8cf3ef3a8f7f5311afb6
Denis <vin_ogr@list.ru>
2024-11-13 14:19:17 +0700

    Распаковка позиционных параметров

--- homework3-3.py
+++ homework3-3.py
@@ -1,13 +1,21 @@
-def test(*params):
-    print(params)
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+values_list = [54.32, 'asd', False]
+values_dict = {'a' : 1, 'b' : 'Popup', 'c' : True}
+values_list_2 = [54.32, 'Str']
 
+print_params()
+print_params(1, 25)
+print_params(1, 'Str',[1, 2, 3])
+print_params(*values_list)
+print_params(**values_dict)
+print_params(values_dict)
+print_params(*values_dict)
+print_params(**values_dict)
+print_params(values_list_2)
+print_params(*values_list_2)
+print_params(*values_list_2, 42)
+print_params(b=25)
+print_params(c = [1,2,3])
 
-def factoria(n):
-    if n == 1:
-        return n
-    else:
-        return n * factoria(n - 1)
-
-
-test('adf', False, 828)
-print(factoria(int(input('Введите число n: '))))
+values_list_2 = [54.32, 'Строка' ]
